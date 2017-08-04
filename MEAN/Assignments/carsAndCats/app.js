6dd277abdd7942de011a107a10fe8ea832d3c56f
Marcus Norona <marcusjeremynorona@gmail.com>
2017-08-03 17:16:11 -0700

    cars and cats

---
+++ MEAN/Assignments/carsAndCats/app.js
@@ -0,0 +1,51 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/cars') {
+        fs.readFile('views/cars.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/carImage') {
+        fs.readFile('images/cars.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/*'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/cats') {
+        fs.readFile('views/cats.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/catsImage') {
+        fs.readFile('images/cats.jpg', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'image/*'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    } else if(request.url === '/cars/new') {
+        fs.readFile('views/carNew.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});  // send data about response
+            response.write(contents);  //  send response body
+            response.end(); // finished!
+        });
+    }
+    
+    // request didn't match anything:
+    else {
+        response.writeHead(404);
+        response.end('Requested URL not available');
+    }
+});
+// tell your server which port to run on
+server.listen(7077);
+// print to terminal window
+console.log("Running in localhost at port 7077");
