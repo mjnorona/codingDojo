d7e0d47adabd34259892a01746a0fbfbd95d29cf
Marcus Norona <marcusjeremynorona@gmail.com>
2017-08-09 18:19:56 -0700

    angular FirstApp

---
+++ MEAN/Assignments/myFirstAngularApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { FormsModule } from '@angular/forms'; // <-- Import FormsModule
+import { HttpModule } from '@angular/http'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
